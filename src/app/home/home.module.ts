a099c284f5314c39fb6738ebb969f7215fe18400
Tony362 <araimundo362@gmail.com>
2021-09-28 21:17:10 -0300

    feat(): adds the items list to home page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { RatingComponent } from '../components/rating/rating.component';
 
 
 @NgModule({
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, RatingComponent]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../interface/movie';
+import { MoviesService } from '../services/Movies/movies.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  loading = false;
+  movies: Movie[];
 
+  constructor(private moviesService: MoviesService) {}
+
+
+  ngOnInit() {
+    this.loading = true;
+    setTimeout(() => {
+      this.movies = this.moviesService.getMovies();
+      this.loading = false;
+    }, 2000);
+  }
+
+  setNewRate = (ev: any, mov: string) => {
+    this.moviesService.rateMovie(mov,ev);
+  };
 }
