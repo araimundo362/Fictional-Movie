d6f92def8fb6cb31b12b53ec2819d9839f607e46
Tony362 <araimundo362@gmail.com>
2021-09-29 15:52:34 -0300

    feat(): adds the path of add movie page to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'add-movie',
+    loadChildren: () => import('./add-movie/add-movie.module').then( m => m.AddMoviePageModule)
+  },
 
 ];
 
